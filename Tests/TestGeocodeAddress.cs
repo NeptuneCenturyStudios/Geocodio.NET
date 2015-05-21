fc9711ba6039d299a800df90d29bf6e0f0e2b71c
Eric Butler <neptunecentury@hotmail.com>
2015-05-21 11:08:04 -0700

    commit

--- Tests/TestGeocodeAddress.cs
+++ Tests/TestGeocodeAddress.cs
@@ -11,9 +11,9 @@ namespace Tests
         {
 
             //create object
-            var geocodio = new Geocodio.Geocodio() { ApiKey = "546744456454a96ab412454693921ef325b15a3" };
+            var geocodio = new Geocodio.Geocodio() { ApiKey = "" };
 
-            var resp = geocodio.GetGeolocation("875 Pone Lane, Franklin, PA 16323");
+            var resp = geocodio.GetGeolocation("60 E Broadway, Bloomington, MN 55425");
 
         }
     }
