d89d674274721e9e1e9110b0aadbd8c600e4fa18
ensemblebd <bdurand@ensemblegroup.net>
2016-08-10 16:37:44 -0500

    Update GeocodioAddressComponents.cs
    cleanup..

--- Geocodio/GeocodioAddressComponents.cs
+++ Geocodio/GeocodioAddressComponents.cs
@@ -16,15 +16,15 @@ namespace Geocodio
         public string County { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-		public string FormattedStreet { get; set; }
+        public string FormattedStreet { get; set; }
     }
-	public class GeocodioAddressRequest {
-		public string number { get; set; }
-		public string street { get; set; }
-		public string city { get; set; }
-		public string county { get; set; }
-		public string state { get; set; }
-		public string zip { get; set; }
-		public string address { get; set; }
-	}
+    public class GeocodioAddressRequest {
+        public string number { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string county { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string address { get; set; }
+    }
 }
